72d8fa1177d78015bae6c1d9df14a2dac3b27d04
aziz abd <aziz.abdullaev@proton.me>
2024-09-21 20:31:05 +0500

    remove django filter backend

--- inson_ai_backend/settings.py
+++ inson_ai_backend/settings.py
@@ -102,7 +102,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework.authentication.SessionAuthentication',
     ),
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PARSER_CLASSES': (
